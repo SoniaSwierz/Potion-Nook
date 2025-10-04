794fad46e03ddfa3fd7c10ade465c9a52ed56ac9
Sonia Świerz <sonia.swierz@gmail.com>
2025-10-04 14:05:46 +0200

    Clear counter visual

--- Potion Nook/Assets/Scripts/Player.cs
+++ Potion Nook/Assets/Scripts/Player.cs
@@ -1,6 +1,16 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+
+    public static Player Instance { get; private set; }
+
+
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangedEventArgs : EventArgs {
+        public ClearCounter selectedCounter;
+    }
+
 
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
@@ -12,32 +22,29 @@ public class Player : MonoBehaviour {
     private float moveDistance;
     private bool isWalking;
     private Vector3 lastInteractDir;
+    private ClearCounter selectedCounter;
+
+
+    private void Awake() {
+        if (Instance != null) {
+            Debug.LogError("More than one Player instance");
+        }
+        Instance = this;
+    }
 
     private void Start () {
         gameInput.OnInteractAction += GemeInput_OnInteractAction;
     }
 
-    private void GemeInput_OnInteractAction(object sender, System.EventArgs e) {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        if (moveDir != Vector3.zero) {
-            lastInteractDir = moveDir;
-        }
-
-        float interactDistance = 2f;
-        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
-            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
-                // Counter is clear
-                clearCounter.Interact();
-
-            }
-        }
-    }
-
     private void Update() {
         HandleMovement();
         HandleInteractions();
+    }
+
+    private void GemeInput_OnInteractAction(object sender, System.EventArgs e) {
+        if (selectedCounter != null) {
+            selectedCounter.Interact();
+        }
     }
 
     private void HandleInteractions() {
@@ -52,8 +59,14 @@ public class Player : MonoBehaviour {
         if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
                 // Counter is clear
-                
+                if (clearCounter != selectedCounter) {
+                    SetSelectedCounter(clearCounter);
+                }
+            } else {
+                SetSelectedCounter(null);
             }
+        } else {
+            SetSelectedCounter(null);
         }
     }
 
@@ -67,7 +80,7 @@ public class Player : MonoBehaviour {
         canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
                                        playerRadius, moveDir, moveDistance);
 
-        if (!canMove) {
+        if (!canMove) { // "Hugging a wall" while moving diagonally
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
             Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
 
@@ -96,5 +109,13 @@ public class Player : MonoBehaviour {
 
     public bool IsWalking() {
         return isWalking;
+    }
+
+    private void SetSelectedCounter(ClearCounter selectedCounter) {
+        this.selectedCounter = selectedCounter;
+
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs {
+            selectedCounter = selectedCounter
+        });
     }
 }
